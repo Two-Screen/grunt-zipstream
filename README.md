fad0002022eddb1f338851fb33d8ec6bb92ee110
Stéphan Kochen <stephan@angrybytes.com>
2013-01-09 12:15:32 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,55 @@
+# grunt-zipstream
+
+Create ZIP files with zipstream.
+
+## Getting Started
+
+Install this grunt plugin next to your project's [grunt.js gruntfile]
+with: `npm install grunt-zipstream`
+
+Then add this line to your project's `grunt.js` gruntfile:
+
+    grunt.loadNpmTasks('grunt-zipstream');
+
+ [grunt.js gruntfile]: https://github.com/gruntjs/grunt/blob/0.3-stable/docs/getting_started.md
+
+## Documentation
+
+A simple multitask called `zip` is defined. Configure it like so:
+
+    grunt.initConfig({
+        zip: {
+            foobar: {
+                src: [
+                    'static/images/**/*''
+                    'static/index.html',
+                    'README.md'
+                ],
+                dest: 'package.zip',
+
+                /* optional */
+                base: 'some/path',
+                subdir: 'some/path',
+                zlib: {
+                    level: 1
+                }
+            }
+        },
+        /* optional */
+        zlib: { /* ... */ }
+    });
+
+The `base` option can be set to a base path that will be stripped off the
+filenames in the zip file. The `subdir` option allows you to add a custom
+prefix to filenames in the zip file, (usually a single directory name.)
+
+Per task, or globally, you can set `zlib` to options for the Node.js `zlib`
+module. See [the `zlib` documentation] for specifics, but the most useful
+will be the `level` option controlling compression level.
+
+ [the `zlib` documentation]: http://nodejs.org/api/zlib.html#zlib_options
+
+## License
+
+Copyright (c) 2013 Stéphan Kochen
+Licensed under the MIT license.
---
+++ tasks/zipstream.js
@@ -0,0 +1,107 @@
+/*
+ * grunt-zipstream
+ * https://github.com/Two-Screen/grunt-zipstream
+ *
+ * Copyright (c) 2013 Stéphan Kochen
+ * Licensed under the MIT license.
+ */
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+var zipstream = require('zipstream');
+
+module.exports = function(grunt) {
+
+    grunt.registerMultiTask('zip', 'Create a ZIP file.', function() {
+        var dest = this.file.dest;
+        var files = grunt.file.expandFiles(this.file.src);
+        var options = this.data;
+
+        var done = this.async();
+        grunt.helper('zipstream', dest, files, options, function(err, written) {
+            if (!err) {
+                written = String(written);
+                grunt.log.writeln("File " + dest.cyan + " created.");
+                grunt.log.writeln('Total size: ' + written.green + ' bytes.');
+            }
+            done();
+        });
+    });
+
+    grunt.registerHelper('zipstream', function(dest, files, options, callback) {
+        var nowrite = grunt.option('no-write');
+        if (typeof(options) === 'function') {
+            callback = options;
+            options = null;
+        }
+        if (!options) {
+            options = {};
+        }
+
+        var base = new RegExp('^' + (options.base || ''));
+        var subdir = options.subdir || '';
+
+        // Helper to call back only once.
+        function callbackOnce(err, arg) {
+            var cb = callback;
+            callback = null;
+            if (cb) { cb(err, arg); }
+        }
+
+        // Helper to create a stream error handler function.
+        function createErrorHandler(verb) {
+            return function(err) {
+                if (!callback) { return; }
+                grunt.verbose.error();
+                grunt.fail.warn("Failed to " + verb + ": " + err.message);
+                callbackOnce(err);
+            };
+        }
+
+        grunt.verbose.write((nowrite ? 'Not actually opening ' : 'Opening ') + dest.cyan + '...');
+        // Create path, if necessary.
+        grunt.file.mkdir(path.dirname(dest));
+        // Create the zip stream.
+        var zip = zipstream.createZip(options.zlib || grunt.config('zlib'));
+        zip.on('error', createErrorHandler('zip'));
+        // Create the destination file stream.
+        if (!nowrite) {
+            var destStream = fs.createWriteStream(dest);
+            destStream.on('error', createErrorHandler('write'));
+            zip.pipe(destStream);
+        }
+        grunt.verbose.ok();
+
+        grunt.util.async.waterfall([
+            function(cb) {
+                grunt.util.async.forEachSeries(files, function(file, cb) {
+                    grunt.verbose.write('Zipping ' +  file.cyan + '...');
+                    // Create the source write stream.
+                    var srcStream = fs.createReadStream(file);
+                    srcStream.on('error', createErrorHandler('read'));
+                    // Transform path.
+                    file = file.replace(base, subdir);
+                    // Zip up.
+                    var obj = { name: file };
+                    zip.addFile(srcStream, obj, function() {
+                        grunt.verbose.ok();
+                        var uncompressed = String(obj.uncompressed);
+                        var compressed = String(obj.compressed);
+                        grunt.verbose.writeln("Compressed " + uncompressed.green + " bytes down to " + compressed.green + " bytes.");
+                        cb();
+                    });
+                }, cb);
+            },
+            function(cb) {
+                grunt.verbose.write('Finalizing zip...');
+                // This waits for writing to finish.
+                zip.finalize(function(written) {
+                    grunt.verbose.ok();
+                    cb(null, written);
+                });
+            }
+        ], callbackOnce);
+    });
+
+};
